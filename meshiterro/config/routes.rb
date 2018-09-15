0fe504ab2df8f5740fd42442c0467b480d821f94
Hiroshige02 <wanyou@manyunoMacBook-puro.local>
2018-09-15 16:34:45 +0900

    [Submit]5-7

---
+++ meshiterro/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  get 'post_comments/create'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "post_images#index"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :post_comments, only: [:create, :destroy]
+   resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+
+end
